dbfe92e409b41f69a441f66f02b725bf541232ce
Jerson Perez <ing.jersonperez@gmail.com>
2021-10-29 13:35:05 -0500

    [Add] social media to all members

--- src/Components/About.js
+++ src/Components/About.js
@@ -5,112 +5,57 @@ class About extends Component {
   render() {
     if (!this.props.data) return null;
 
-    const name1 = this.props.data.name1;
-    const name2 = this.props.data.name2;
-    const name3 = this.props.data.name3;
-    const profilepic1 = "images/" + this.props.data.image1;
-    const profilepic2 = "images/" + this.props.data.image2;
-    const profilepic3 = "images/" + this.props.data.image3;
-    const bio1 = this.props.data.bio1;
-    const bio2 = this.props.data.bio2;
-    const bio3 = this.props.data.bio3;
-    const city1 = this.props.data.address.city1;
-    const state1 = this.props.data.address.state1;
-    const city2 = this.props.data.address2.city2;
-    const state2 = this.props.data.address2.state2;
-    const email1 = this.props.data.email1;
-    const email2 = this.props.data.email2;
-    const email3 = this.props.data.email3;
+    const team = this.props.data.team;
+
+    const printSocial = function(user){
+      return (user.social.map(function (network) {
+        return (
+          <li key={network.name}>
+            <a href={network.url}>
+              <i className={network.className}></i>
+            </a>
+          </li>
+        );
+      }));
+    };
+
+    const users = team.map(function (user) {
+      return (
+        <div className="row">
+          <div className="three columns">
+            <img
+              className="profile-pic"
+              src={"images/" + user.image}
+              alt={user.name}
+            />
+          </div>
+          <div className="nine columns main-col">
+            <h2>{user.name}</h2>
+
+            <span>
+            {user.desc} from {user.address.city}, {user.address.state}
+            </span>
+            <p>{user.bio}</p>
+
+            <div className="row">
+              <div className="columns contact-details">
+                <h4>Contact Details</h4>
+                <ul className="social-links">
+                {printSocial(user)}
+                </ul>
+              </div>
+            </div>
+
+            <br />
+          </div>
+        </div>
+      )
+    });
+    
 
     return (
       <section id="about">
-        <Fade duration={1000}>
-          <div className="row">
-            <div className="three columns">
-              <img
-                className="profile-pic"
-                src={profilepic1}
-                alt="Salome"
-              />
-            </div>
-            <div className="nine columns main-col">
-              <h2>Salomé Grisales</h2>
-
-              <p>{bio1}</p>
-              <div className="row">
-                <div className="columns contact-details">
-                  <h2>Contact Details</h2>
-                  <p className="address">
-                    <span>{name1}</span>
-                    <br />
-                    <span>
-                      {city1}, {state1}
-                    </span>
-                    <br />
-                    <span>{email1}</span>
-                  </p>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <div className="row">
-            <div className="three columns">
-              <img
-                className="profile-pic"
-                src={profilepic2}
-                alt="Jerson"
-              />
-            </div>
-            <div className="nine columns main-col">
-              <h2>Jerson Perez</h2>
-
-              <p>{bio2}</p>
-              <div className="row">
-                <div className="columns contact-details">
-                  <h2>Contact Details</h2>
-                  <p className="address">
-                    <span>{name2}</span>
-                    <br />
-                    <span>
-                      {city1}, {state2}
-                    </span>
-                    <br />
-                    <span>{email2}</span>
-                  </p>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="row">
-            <div className="three columns">
-              <img
-                className="profile-pic"
-                src={profilepic3}
-                alt="Sebastian"
-              />
-            </div>
-            <div className="nine columns main-col">
-              <h2>Sebastian Molina</h2>
-
-              <p>{bio3}</p>
-              <div className="row">
-                <div className="columns contact-details">
-                  <h2>Contact Details</h2>
-                  <p className="address">
-                    <span>{name3}</span>
-                    <br />
-                    <span>
-                      {city2}, {state2}
-                    </span>
-                    <br />
-                    <span>{email3}</span>
-                  </p>
-                </div>
-              </div>
-            </div>
-          </div>
-        </Fade>
+        <Fade duration={1000}>{users}</Fade>
       </section>
     );
   }
